30cfcd4860bacf6dabeee1ffac0759472cf61449
Art <WildandArt@users.noreply.github.com>
2022-06-16 20:08:12 +0300

    Initial Commit

---
+++ TableView with Nibs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView with Nibs
+//
+//  Created by Artemy Ozerski on 16/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
